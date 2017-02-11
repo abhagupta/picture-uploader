5ea3e140a1bb33ddf88dfe66430ac2bd8e0806d9
Abha Gupta <agupta3@walmart.com>
2017-02-11 15:01:34 -0800

    make it better

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express();
+var bodyParser = require('body-parser');
+var busboy = require('connect-busboy');
+
+var path = require('path'),
+    fs = require('fs');
+
+//app.use(bodyParser({uploadDir:'/upload'}));
+app.use(busboy());
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+app.post('/upload', function (req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+        fstream = fs.createWriteStream(__dirname + '/public/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            res.redirect('back');
+        });
+    });
+    // ...
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
